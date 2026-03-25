1844869ec551881bf407c1956a35a072cdf5e22f
SeanAndrie <gseanandrie@gmail.com>
2026-03-25 14:39:47 +0400

    chore: separated normal and rgb assignment

--- src/render/core/render_trace.c
+++ src/render/core/render_trace.c
@@ -14,57 +14,60 @@
 #include <float.h>
 #include <core/render.h>
 
-static bool	fill_hit(t_ray *ray, t_isect *is, t_hit *hit)
+static void	assign_rgb(t_hit *hit)
 {
-	if (!is->obj)
-		return (false);
-	hit->t = is->t;
-	hit->obj = is->obj;
-	hit->point = ray_at(*ray, hit->t);
-	if (is->obj->type == OBJ_SPHERE)
-	{
-		hit->normal = normal_sphere(hit->point, &is->obj->data.sphere);
-		hit->rgb = is->obj->data.sphere.rgb;
-	}
-	else if (is->obj->type == OBJ_PLANE)
-	{
-		hit->normal = normal_plane(ray->dir, &is->obj->data.plane);
-		hit->rgb = is->obj->data.plane.rgb;
-	}
-	else if (is->obj->type == OBJ_CYLINDER)
-	{
-		hit->normal = normal_cylinder(hit->point, &is->obj->data.cylinder,
+	if (hit->obj->type == OBJ_SPHERE)
+		hit->rgb = hit->obj->data.sphere.rgb;
+	else if (hit->obj->type == OBJ_PLANE)
+		hit->rgb = hit->obj->data.plane.rgb;
+	else if (hit->obj->type == OBJ_CYLINDER)
+		hit->rgb = hit->obj->data.cylinder.rgb;
+	else if (hit->obj->type == OBJ_CONE)
+		hit->rgb = hit->obj->data.cone.rgb;
+}
+
+static void	assign_normal(t_vec3 ray_dir, t_hit *hit)
+{
+	if (hit->obj->type == OBJ_SPHERE)
+		hit->normal = normal_sphere(hit->point, &hit->obj->data.sphere);
+	else if (hit->obj->type == OBJ_PLANE)
+		hit->normal = normal_plane(ray_dir, &hit->obj->data.plane);
+	else if (hit->obj->type == OBJ_CYLINDER)
+		hit->normal = normal_cylinder(hit->point, &hit->obj->data.cylinder,
 				hit->t);
-		hit->rgb = is->obj->data.cylinder.rgb;
-	}
-	else if (is->obj->type == OBJ_CONE)
-	{
-		hit->normal = normal_cone(hit->point, &is->obj->data.cone, hit->t);
-		hit->rgb = is->obj->data.cone.rgb;
-	}
+	else if (hit->obj->type == OBJ_CONE)
+		hit->normal = normal_cone(hit->point, &hit->obj->data.cone, hit->t);
+}
+
+static bool	fill_hit(t_ray *ray, t_hit *hit)
+{
+	if (!hit->obj)
+		return (false);
+	hit->point = ray_at(*ray, hit->t);
+	assign_normal(ray->dir, hit);
+	assign_rgb(hit);
 	return (true);
 }
 
 bool	render_trace(t_ray *ray, t_hit *hit, t_scene *scene)
 {
-	t_isect		is;
 	t_object	*curr;
 	float		hit_t;
 
-	is.obj = NULL;
-	is.t = FLT_MAX;
+	hit->obj = NULL;
+	hit->t = FLT_MAX;
 	curr = scene->objects;
 	while (curr)
 	{
 		hit_t = isect_obj(ray, curr);
-		if (hit_t > 1e-4f && hit_t < is.t)
+		if (hit_t > 1e-4f && hit_t < hit->t)
 		{
-			is.t = hit_t;
-			is.obj = curr;
+			hit->t = hit_t;
+			hit->obj = curr;
 		}
 		curr = curr->next;
 	}
-	if (!fill_hit(ray, &is, hit))
+	if (!fill_hit(ray, hit))
 		return (false);
 	return (true);
 }
