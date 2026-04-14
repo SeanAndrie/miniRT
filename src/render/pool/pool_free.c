52f5025ff0abb3f5d5cd767a63351264e6a501b1
SeanAndrie <gseanandrie@gmail.com>
2026-04-14 10:27:39 +0400

    fix: tiles array memory leak

--- src/render/pool/pool_free.c
+++ src/render/pool/pool_free.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 02:52:33 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/04/02 02:53:36 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/04/14 10:16:31 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ void	pool_free(t_pool *pool)
 	if (pool->workers)
 		free(pool->workers);
 	if (pool->tiles)
+	{
 		tile_free(pool->tiles, pool->n_tiles);
+		free(pool->tiles);
+	}
 }
