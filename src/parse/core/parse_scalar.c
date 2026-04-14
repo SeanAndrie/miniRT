c79d1c5c83b8ab96f5eeb9a4fa4b18a926852336
SeanAndrie <gseanandrie@gmail.com>
2026-04-14 15:00:03 +0400

    feat: use ft_printf with float support for range error

--- src/parse/core/parse_scalar.c
+++ src/parse/core/parse_scalar.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 17:17:19 by sgadinga          #+#    #+#             */
-/*   Updated: 2026/04/14 10:13:52 by sgadinga         ###   ########.fr       */
+/*   Updated: 2026/04/14 14:56:35 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,12 @@
 #include <libft.h>
 #include <setup/parse.h>
 
-static void	print_float(double val)
-{
-	int	integer;
-	int	fraction;
-
-	if (val < 0)
-	{
-		ft_dprintf(STDERR_FILENO, "-");
-		val = -val;
-	}
-	if (val > 2147483647.0)
-	{
-		ft_dprintf(STDERR_FILENO, "inf");
-		return ;
-	}
-	integer = (int)val;
-	fraction = (int)((val - integer) * 10000);
-	ft_dprintf(STDERR_FILENO, "%d.%01d", integer, fraction);
-}
-
 static void	range_error(const char *nptr, double min, double max)
 {
 	ft_dprintf(STDERR_FILENO, "Error\n");
-	log_error(ERR_WARNING, ERR_BASE, "value '%s' is out of range [", nptr);
-	print_float(min);
-	ft_dprintf(STDERR_FILENO, ", ");
-	print_float(max);
-	ft_dprintf(STDERR_FILENO, "]\n");
+	ft_dprintf(STDERR_FILENO, "%s: warning: ", ERR_BASE);
+	ft_dprintf(STDERR_FILENO, "value '%s' is out of range [%.2f, %.2f]\n", nptr,
+		min, max);
 }
 
 t_bool	parse_scalar(const char *nptr, float min, float max, float *n)
